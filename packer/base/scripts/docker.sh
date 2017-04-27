14e36103657408ff079221925dd325121042d4d1
Wiliam Souza <wiliamsouza83@gmail.com>
2017-04-27 17:53:05 -0300

    Update to use docker-ce

--- packer/base/scripts/docker.sh
+++ packer/base/scripts/docker.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 set -e
 
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-echo 'deb https://apt.dockerproject.org/repo ubuntu-xenial main' > /etc/apt/sources.list.d/docker.list
+apt-get update -y
+
+sudo apt-get install \
+	apt-transport-https \
+	ca-certificates \
+	curl \
+	software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+apt-key fingerprint 0EBFCD88
+
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
 
 apt-get update -y
-apt-get purge -y lxc-docker
-apt-cache policy docker-engine
 
-apt-get install -o Dpkg::Options::="--force-confold" -y \
-        linux-image-extra-$(uname -r) \
-        docker-engine
+apt-get install -y -o Dpkg::Options::="--force-confold" docker-ce
+
+apt-get autoremove -y
 
 gpasswd -a ubuntu docker
 
